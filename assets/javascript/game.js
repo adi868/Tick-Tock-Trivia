50486926ada7370b97bf007e76918e37c65478bc
alciancio <alcianciotto@gmail.com>
2019-05-30 00:24:43 -0500

    buttons selector

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,55 +24,55 @@ var questionAnswers = [{
     },
     {
         question: "Which European country was reunified in 1990?",
-        answers: ["1. Austria", "2. Russia", "3. Switzerland", "4. Germany"],
+        answers: ["Austria", "Russia", "Switzerland", "Germany"],
         rightAnswer: "Germany",
         animate: "<img src='https://media.giphy.com/media/J8oFJS9S9yaAw/giphy.gif' width='500px' height='300px'/>"
     },
     {
         question: "Which Desert dominates a large area of Northern Africa?",
-        answers: ["1. Kalahari", "2. Sahara", "3. Gobi", "4. Atacama"],
+        answers: ["Kalahari", "Sahara", "Gobi", "Atacama"],
         rightAnswer: "Sahara",
         animate: "<img src='https://media.giphy.com/media/7zGpUAi5TOCGme7KtI/giphy.gif' width='500px' height='300px'/>"
     },
     {
         question: "What is the formal language of Libya?",
-        answers: ["1. English", "2. Spanish", "3. Arabic", "4. Dutch"],
+        answers: ["English", "Spanish", "Arabic", "Dutch"],
         rightAnswer: "Arabic",
         animate: "<img src='https://media.giphy.com/media/J93sVmfYBtsRi/giphy.gif' width='500px' height='300px'/>"
     },
     {
         question: "In which Canadian province is Montreal located?",
-        answers: ["1. Vancouver", "2. Ontario", "3.Quebec", "4. Nova Scotia"],
+        answers: ["Vancouver", "Ontario", "Quebec", "Nova Scotia"],
         rightAnswer: "Quebec",
         animate: "<img src='https://media.giphy.com/media/Jx9n9pSg9JUJi/giphy.gif' width='500px' height='300px'/>"
     },
     {
         question: "Bronze is an alloy consisting of two or more metals. What are they?",
-        answers: ["1. Copper and Zinc", "2. Copper and Tin", "3. Copper and Gold", "4. Copper and Lead"],
+        answers: ["Copper and Zinc", "Copper and Tin", "Copper and Gold", "Copper and Lead"],
         rightAnswer: "Copper and Tin",
         animate: "<img src='https://media.giphy.com/media/fqIBaMWI7m7O8/giphy.gif' width='500px' height='300px'/>"
     },
     {
         question: "Mycology is the study of:	",
-        answers: ["1. Insects", "2. Water bodies", "3. Fungi", "4. Volcanos"],
+        answers: ["Insects", "Water bodies", "Fungi", "Volcanos"],
         rightAnswer: "Fungi",
         animate: "<img src='https://media.giphy.com/media/d0XuEToIwURJ6/giphy.gif' width='500px' height='300px'/>"
     },
     {
         question: "What is the speed of light?",
-        answers: ["1. 299,792,458 metres per second", "2. 3.14159265359 metres per second", "3. 967,268,439 kilometres per second", "4. 3,598,689,696,123 kilometres per second"],
+        answers: ["299,792,458 metres per second", "3.14159265359 metres per second", "967,268,439 kilometres per second", "3,598,689,696,123 kilometres per second"],
         rightAnswer: "299,792,458 metres per second",
         animate: "<img src='https://media.giphy.com/media/vKOY98CLBQk9O/giphy.gif' width='500px' height='300px'/>"
     },
     {
         question: "What is the currency of Sweden?",
-        answers: ["1. Euro", "2. Krona", "3. Franc", "4. Pound"],
+        answers: ["Euro", "Krona", "Franc", "Pound"],
         rightAnswer: "Krona",
         animate: "<img src='https://media.giphy.com/media/ND6xkVPaj8tHO/giphy.gif' width='500px' height='300px'/>"
     },
     {
         question: "For which of the following disciplines is Nobel Prize awarded?",
-        answers: ["1. Physics and Chemistry", "2. Physiology or Medicine", "3. Literature, Peace and Economics", "4. All of the above"],
+        answers: ["Physics and Chemistry", "Physiology or Medicine", "Literature, Peace and Economics", "All of the above"],
         rightAnswer: "All of the above",
         animate: "<img src='https://media.giphy.com/media/19U2aQ323iGtO/giphy.gif' width='500px' height='300px'/>"
     },
@@ -84,18 +84,18 @@ function showTrivia() {
         startTimer();
         $("#button").empty();
         $("#showQuestion").html(questionAnswers[index].question);
-        $("#showPossibleAnswers").html(questionAnswers[index].answers);
         $("#image-holder").html(questionAnswers[index].animate);
-        // console.log(questionAnswers[index].answers[index]);
-        // console.log(questionAnswers[index].answers[2])
-        setTimeout(generateAnswers, 8000);
-        // $("#showPossibleAnswers").attr("data-answer", num2);
+        $("#choice1").html(questionAnswers[index].answers[0]);
+        $("#choice2").html(questionAnswers[index].answers[1]);
+        $("#choice3").html(questionAnswers[index].answers[2]);
+        $("#choice4").html(questionAnswers[index].answers[3]);
+        $(".choice").on("click", function () {
+            generateAnswers();
+            checkAnswer();
+        });
+         console.log(questionAnswers[index].answers[index]);
+         console.log(questionAnswers[index].answers[2])
         // checkAnswer();
-        // $("#showPossibleAnswers").on("click", function () {
-        //     alert($("#showPossibleAnswers").index(questionAnswers[index].answers));
-
-        // alert("hey!");
-        //       alert(questionAnswers[index].answers[1]);
     }
 }
 
